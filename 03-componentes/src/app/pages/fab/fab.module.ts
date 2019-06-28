3c1d7bd2a3ce710de0c63f0314bdd32071608e73
Angel Alberto Dauzon Martinez <adauzon@in.sev.gob.mx>
2019-06-28 18:24:58 -0500

    Fin de semana

--- 03-componentes/src/app/pages/fab/fab.module.ts
+++ 03-componentes/src/app/pages/fab/fab.module.ts
@@ -8,7 +8,7 @@ import { IonicModule } from '@ionic/angular';
 import { FabPage } from './fab.page';
 import { ComponentsModule } from '../../components/components.module';
 
-const routes: Routes = [
+const routes: Routes = [ 
   {
     path: '',
     component: FabPage
--- 03-componentes/src/app/pages/infinite-scroll/infinite-scroll.page.ts
+++ 03-componentes/src/app/pages/infinite-scroll/infinite-scroll.page.ts
@@ -11,17 +11,8 @@ export class InfiniteScrollPage implements OnInit {
   data = Array(20);
   ngOnInit() {
   }
-  loadData(event) {
-    setTimeout(() => {
-      console.log('Done');
-      event.target.complete();
-
-      // App logic to determine if all data is loaded
-      // and disable the infinite scroll
-      if (data.length == 1000) {
-        event.target.disabled = true;
-      }
-    }, 500);
+  loadData() {
+  console.log('0FFF')
   }
 }
  
